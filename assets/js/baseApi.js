329444cae1ff94a7889836de13c7efb2ce94fd82
ittao <120697869@qq.com>
2020-07-18 12:46:30 +0800

    2222

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -4,6 +4,23 @@ $(function () {
     // 在这个函数中，可以拿到我们给Ajax提供的配置对象
     $.ajaxPrefilter(function (options) {
         options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    })
 
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        };
+
+        // 不论成功还是失败，最终都会调用 complete 回调函数
+        options.complete = function (res) {
+            // console.log(res);
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 强制清空token
+                localStorage.removeItem('token');
+                // 强制跳转到登录页
+                location.href = '/home/login.html'
+            }
+        }
+
+    })
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,59 @@
 $(function () {
     getUser()
+
+    var layer = layui.layer;
+
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确认退出登录?', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token');
+            location.href = '/home/login.html';
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
 })
 
+// 获取用户信息
 function getUser() {
     $.ajax({
         method: 'get',
         url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || '',
-        },
+
         success: function (res) {
-            if (res.stauts !== 0) return layui.layer.msg('获取用户信息失败！');
+            if (res.status !== 0) {
+                layui.layer.msg('获取用户信息失败！')
+                return console.log(res);
+            };
             console.log(res);
+            renderAvatar(res.data)
+
         }
     })
 }
+
+
+function renderAvatar(user) {
+    // 获取用户名称
+    var name = user.nickname || user.username;
+    console.log(name)
+    // 设置欢迎文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show()
+        $('.text-avatar').hide()
+    } else {
+        // 3.2 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar')
+            .html(first)
+            .show()
+    }
+}
+
+
+
